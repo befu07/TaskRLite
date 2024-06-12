8608f24361f11a41e57453e0d9c3ed56e3016d9b
Bernhard <Bernhard@DESKTOP>
2024-06-12 21:58:07 +0200

    AppUser: Added Salt, Hash Fields  +Migration +dbupdate

--- TaskRLite/Data/AppUser.cs
+++ TaskRLite/Data/AppUser.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public byte[] Hash { get; set; }
+        public byte[] Salt { get; set; }
     }
 }
---
+++ TaskRLite/Migrations/20240612195318_HashSalt.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskRLite.Migrations
+{
+    /// <inheritdoc />
+    public partial class HashSalt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Hash",
+                table: "AppUsers",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Salt",
+                table: "AppUsers",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hash",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "AppUsers");
+        }
+    }
+}
